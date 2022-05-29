c8cbf42935d5f7011387ae2e66a6c92c225d6876
Cuda <104436099+Mr-Cuda@users.noreply.github.com>
2022-05-29 16:52:55 +0200

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,6 @@ from pytube import YouTube
 link = input("Enter a YouTube link: ")
 yt = YouTube(link)
 
-yt = yt.streams.get_higest_resolution()
+yt = yt.streams.get_highest_resolution()
 ys.download()
 print("Download Completed!")
