56c2c3f4087235ee5e22721d9326be02d9d741ac
Cuda <104436099+Mr-Cuda@users.noreply.github.com>
2022-05-29 17:34:16 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
+class color:
+   GREEN = '\033[92m'
+   RED = '\033[91m'
+   
 from pytube import YouTube
+from colorama import init
+init(autoreset=True)
+
 
 link = input("Enter a YouTube link: ")
 yt = YouTube(link)
 
 ys = yt.streams.get_highest_resolution()
 ys.download()
-print("Download Completed!")
+print(f" ")
+print(color.GREEN + f' Download Completed!')
+print(f" ")
