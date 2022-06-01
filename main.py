06c10709df9bc44f0dbec5ce2ee383256180b90e
Cuda <104436099+Mr-Cuda@users.noreply.github.com>
2022-06-01 15:58:00 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,11 @@
 class color:
    GREEN = '\033[92m'
-   RED = '\033[91m'
    
 from pytube import YouTube
 from colorama import init
-init(autoreset=True)
 
 
-link = input("Enter a YouTube link: ")
+link = input('Enter a YouTube link: ')
 yt = YouTube(link)
 
 ys = yt.streams.get_highest_resolution()
